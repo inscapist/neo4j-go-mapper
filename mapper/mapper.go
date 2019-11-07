05013f7aa97d7c553b603e60edbe15ca5021994d
Felix <felix@intelllex.com>
2019-11-07 23:13:32 +0800

    Combine related code

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -37,3 +37,127 @@ type Mapper interface {
 	// Use this to run `CREATE INDEX/CONSTRAINTS`
 	Bootstrap(cypherStmts []string) error
 }
+
+type Client struct {
+	driver neo4j.Driver
+}
+
+func NewClient(uri, user, password string) (Mapper, error) {
+	driver, err := neo4j.NewDriver(uri, neo4j.BasicAuth(user, password, ""))
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		driver,
+	}, nil
+}
+
+func (c *Client) Ping() error {
+	return c.Exec("MATCH (n) RETURN n LIMIT 1", nil)
+}
+
+func (c *Client) Exec(cypher string, params map[string]interface{}) error {
+	var (
+		err     error
+		session neo4j.Session
+	)
+	if session, err = c.driver.Session(neo4j.AccessModeWrite); err != nil {
+		return err
+	}
+	defer session.Close()
+
+	if _, err = session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+		result, err := tx.Run(cypher, params)
+		if err != nil {
+			return nil, err
+		}
+		return result.Consume()
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Query is a low level function to read row results. Use the readers from `reader.go` whenever possible
+func (c *Client) Query(
+	cypher string,
+	params map[string]interface{},
+	transform func(record neo4j.Record) interface{},
+) ([]interface{}, error) {
+	var (
+		items   []interface{}
+		err     error
+		session neo4j.Session
+		result  neo4j.Result
+	)
+
+	if session, err = c.driver.Session(neo4j.AccessModeWrite); err != nil {
+		return nil, err
+	}
+	defer session.Close()
+
+	if result, err = session.Run(cypher, params); err != nil {
+		return nil, err
+	}
+
+	for result.Next() {
+		if transform != nil {
+			items = append(items, transform(result.Record()))
+		} else {
+			items = append(items, result.Record())
+		}
+	}
+
+	if err = result.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+// Query is a low level function to read a single row. Use the readers from `reader.go` whenever possible
+func (c *Client) QuerySingle(
+	cypher string,
+	params map[string]interface{},
+	transform func(record neo4j.Record) interface{},
+) (interface{}, error) {
+	var (
+		item    interface{}
+		err     error
+		session neo4j.Session
+		result  neo4j.Result
+	)
+
+	if session, err = c.driver.Session(neo4j.AccessModeWrite); err != nil {
+		return nil, err
+	}
+	defer session.Close()
+
+	if result, err = session.Run(cypher, params); err != nil {
+		return nil, err
+	}
+
+	result.Next()
+	if transform != nil {
+		item = transform(result.Record())
+	} else {
+		item = result.Record()
+	}
+
+	if err = result.Err(); err != nil {
+		return nil, err
+	}
+	return item, nil
+}
+
+func (g *Client) Bootstrap(cypherStmts []string) error {
+	for _, stmt := range cypherStmts {
+		if err := g.Exec(stmt, nil); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Client) Close() error {
+	return c.driver.Close()
+}
